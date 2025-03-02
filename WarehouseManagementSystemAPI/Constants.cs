3c75df48765b4459172619006cf3f025b86cf067
Orkhan <mehbaliyevorxan2003@gmail.com>
2025-03-02 21:51:25 +0400

    Initial commit with solution and multiple projects

---
+++ WarehouseManagementSystemAPI/Constants.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WarehouseManagementSystemAPI
+{
+    public class Constants
+    {
+        public const string DateFormat = "dd MMM yyyy hh:mm:ss t";
+    }
+}
